cab7574f443ebf1b7c02a25f6461b5318497466d
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-11-02 22:49:08 -0400

    seeds.sql base code for schema

---
+++ Main/db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ("Jedi"),
+       ("Jedi"),
+       ("Jedi"),
+       ("Politian"),
+       ("Sith");
+
+INSERT INTO role (title, salary, department_id)
+VALUES (""),
+       (""),
+       (""),
+       (""),
+       ("");       
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Qui Gon", "Jin"),
+       ("Obi Wan", "Kenobi"),
+       ("Anakin", "Skywalker"),
+       ("Padme", "Amadala"),
+       ("Palpatine");
